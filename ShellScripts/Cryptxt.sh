26bbe3d27e15239275969725f829a4684ac35869
Iam iAGorynT <dangers_bunk.0n@icloud.com>
2023-01-19 08:01:51 -0600

    Create Cryptxt.sh
    Added new OpenSSL Text Encryption / Decryption script.

---
+++ ShellScripts/Cryptxt.sh
@@ -0,0 +1,33 @@
+#!/bin/zsh
+#Usage
+#cryptxt.sh -m 'enc' -s 'string' -p 'pass'
+#cryptxt.sh -m 'dec' -s 'U2FsdGVkX18lO/DQQ6Aze5ChxQi4USDFO8jz1ywsWR0=' -p 'pass'
+
+#Get the parameteres of the script and assign them
+while getopts m:s:p: flag
+do
+    case "${flag}" in
+        m) mechanism=${OPTARG};;
+        s) string=${OPTARG};;
+        p) password=${OPTARG};;
+    esac
+done
+
+#Check if all parameters are set, if not show an error message and exit the script
+if [ -z "$mechanism" ] || [ -z "$string" ] || [ -z "$password" ]
+    then echo "You need to set all variables to run the script: -m enc for encryption or dec for decryption, -s The string to encrypt/decrypt, -p The password for the encryption/decryption"
+    exit 0
+fi
+
+#if the mechanism is encryption => encrypt the string, if the mechanism is decryption => decrypt the string
+if [ $mechanism = 'enc' ]
+    then
+    echo "ENCODE $string"
+    echo $string | openssl enc -base64 -e -aes-256-cbc -salt -pass pass:$password -pbkdf2 -iter 100000
+elif [ $mechanism = 'dec' ]
+    then
+    echo "DECODE $string"
+    echo $string | openssl enc -base64 -d -aes-256-cbc -salt -pass pass:$password -pbkdf2 -iter 100000
+else
+    echo "Mechanism (-m) must be enc for encryption or dec for decryption"
+fi
