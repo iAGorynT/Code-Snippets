7021c9433671fb8a1a846be72b9bc574cad25208
Iam iAGorynT <dangers_bunk.0n@icloud.com>
2023-01-28 20:14:04 -0600

    Create Crypvault.sh
    Added OpenSSL Crypt Script.

---
+++ ShellScripts/Crypvault.sh
@@ -0,0 +1,34 @@
+#!/bin/zsh
+
+clear
+echo "OpenSSL Vault Encrypt / Decrypt"
+echo " "
+
+# Testing On Desktop
+cd $HOME/Desktop
+
+# Load Hash_CFG
+# Set Secret Type
+TYPE="Hash_CFG"
+# Set Account
+ACCT=$USER
+
+# Lookup Secret in Keychain
+if ! SECRET=$(security find-generic-password -w -s "$TYPE" -a "$ACCT"); then
+  echo "Secret Not Found, error $?"
+  exit 1
+fi
+
+# Set Vault Name
+VAULT="VcaSutL6TsVSj7IrEmOve"
+
+# Encrypt Vault
+
+#tar -cf $VAULT.tar $VAULT && gzip $VAULT.tar && openssl enc -base64 -e -aes-256-cbc -salt -pass pass:$SECRET -pbkdf2 -iter 100000 -in $VAULT.tar.gz -out $VAULT.enc && rm -f $VAULT.tar.gz
+
+# Decrypt Vault
+
+openssl enc -base64 -d -aes-256-cbc -salt -pass pass:$SECRET -pbkdf2 -iter 100000 -in $VAULT.enc -out $VAULT.tar.gz && tar -xzf $VAULT.tar.gz && rm -f $VAULT.tar.gz 
+
+echo "Vault Encrypted!"
+echo " "
