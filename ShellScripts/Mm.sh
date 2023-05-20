870434d6f4d09144cf16783558279c1870c0391e
Iam iAGorynT <108758623+iAGorynT@users.noreply.github.com>
2023-05-20 18:47:01 -0500

    Added Main Menu
    Simplified System Activities by incorporating multiple menus into a new Main Menu.

---
+++ ShellScripts/Mm.sh
@@ -0,0 +1,50 @@
+#!/bin/zsh
+
+# Trap Ctl-C and Require a Menu Selection to Exit Script
+trap 'echo -e  "\nCtrl-C will not terminate $0."'  INT
+
+function brewm {
+	clear
+	brewm.sh
+}
+
+function opsm {
+	clear
+	opsm.sh
+}
+
+function menu {
+	clear
+	echo
+	echo -e "\t\t\tMain Menu\n"
+	echo -e "\t1. Homebrew Menu"
+	echo -e "\t2. Ops Menu"
+	echo -e "\t0. Exit Menu\n\n"
+	echo -en "\t\tEnter an Option: "
+	read -k 1 option
+}
+
+while [ 1 ]
+do
+	menu
+	case $option in
+	0)
+	break ;;
+
+	1)
+	brewm;;
+
+	2)
+	opsm;;
+
+	*)
+	clear
+	echo "Sorry, wrong selection";;
+	esac
+	echo -en "\n\n\t\t\tHit any key to continue"
+	read -k 1 line
+done
+clear
+
+# Reset Trap Ctl-C
+trap INT
