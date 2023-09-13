f141409414689db27daf25ab439890bb6e81d693
Iam iAGorynT <108758623+iAGorynT@users.noreply.github.com>
2023-09-12 19:38:28 -0500

    Added New Vault Encryption
    Revamped / created all new Paranoia PFE and OpenSSL Vault Encryption scripts.

--- ShellScripts/Crypvault.sh
+++ ShellScripts/Crypvault.sh
@@ -4,36 +4,95 @@ clear
 echo "OpenSSL Vault Encrypt / Decrypt"
 echo " "
 
-# Testing On Desktop
+# Set Default Dir To Desktop
 cd $HOME/Desktop
 
-# Request User Input
-while true; do
-    read action\?"enc / dec: "
-    case $action in
-        [enc]* ) break;;	# Encode vault
-        [dec]* ) break;;	# Decode vault
-        * ) echo "Please answer enc or dec";;
+# Select Action
+echo "Select Action..."
+echo " "
+select act in "Encrypt" "Decrypt" "Config" "Quit"; do
+    case $act in
+        Encrypt ) action="enc"; break;;
+	Decrypt ) action="dec"; break;;
+	Config  ) action="conf"; break;;
+	Quit    ) action="quit"; break;;
     esac
 done
 
-while true; do
-    read vaultname\?"vmgr / gmgr: "
-    case $vaultname in
-        [vmgr]* ) break;;	# VaultMGR
-        [gmgr]* ) break;;	# GitMGR
-        * ) echo "Please answer vmgr or gmgr";;
+# Run ConfigFILES Shortcuts App
+if [ $action = "conf" ]
+    then
+    echo " "
+    echo "Standby... ConfigFILES Running"
+    shortcuts run "ConfigFILES"
+    echo "ConfigFILES Complete..."
+    echo " "
+    exit 0
+# Quit Execution of Script
+elif [ $action = "quit" ]
+    then
+    echo " "
+    exit 0
+fi
+    
+# Select Vault Type
+echo " "
+echo "Select Vault Type..."
+echo " "
+select vnam in "VaultMGR" "GitMGR"; do
+    case $vnam in
+        VaultMGR ) vaultname="vmgr"; break;;
+	GitMGR   ) vaultname="gmgr"; break;;
     esac
 done
+echo " "
+
+# Confirm that Vault ConfigFILE Encryption Type is OpenSSL
+if [ $vaultname = "vmgr" ]
+    then
+    configfile=$HOME'/Library/Mobile Documents/iCloud~is~workflow~my~workflows/Documents/Config Files/vaultmgr_config.json'
+elif [ $vaultname = "gmgr" ]
+    then
+    configfile=$HOME'/Library/Mobile Documents/iCloud~is~workflow~my~workflows/Documents/Config Files/gitmgr_config.json'
+fi
+if grep -q -w PFE $configfile
+    then 
+    echo " "
+    echo "Vault Encryption Type is Paranoia PFE, Use VaultMGR or GitMGR..."
+    echo " "
+    exit 1
+fi
 
 # Set Vault Variables
 if [ $vaultname = "vmgr" ] 
     then
-    vaultdir="VcaSutL6TsVSj7IrEmOve" 
-    vaultenc=$vaultdir".enc";
+# Search VaultMGR JSON Config. File for Filename
+    str=$(cat $HOME'/Library/Mobile Documents/iCloud~is~workflow~my~workflows/Documents/Config Files/vaultmgr_config.json')
+    substr="vaultmgr_name"
+    prefix=${str%%$substr*}
+    index=${#prefix}
+    nameidx=$((index + 16)) # VaultMGR
+# Create a SubString That Begins With VaultMGR Filename
+    str2=$(echo $str | cut -c $nameidx-)
+# Strip Special Characters and Save Actual VaultMGR Filename
+    filename=$(echo $str2 | cut -d '"' -f2)
+# Initialize VaultMGR Names
+    vaultdir=$filename
+    vaultenc=$vaultdir".enc"
 elif [ $vaultname = "gmgr" ] 
     then
-    vaultdir="GciSttH6UsbSj7IrEMoVe"
+# Search GitMGR JSON Config. File for Filename
+    str=$(cat $HOME'/Library/Mobile Documents/iCloud~is~workflow~my~workflows/Documents/Config Files/gitmgr_config.json')
+    substr="gitmgr_name"
+    prefix=${str%%$substr*}
+    index=${#prefix}
+    nameidx=$((index + 14)) # GitMGR
+# Create a SubString That Begins With GitMGR Filename
+    str2=$(echo $str | cut -c $nameidx-)
+# Strip Special Characters and Save Actual GitMGR Filename
+    filename=$(echo $str2 | cut -d '"' -f2)
+# Initialize GitMGR Names
+    vaultdir=$filename
     vaultenc=$vaultdir".enc"
 fi
 
@@ -52,6 +111,14 @@ fi
 # Set File Hash
 hash_cfg=$(echo $secret | base64 --decode)
 
+# If Decrypting Vault, Move Vault from iCloud to Desktop
+iclouddir=$HOME'/Library/Mobile Documents/com~apple~CloudDocs'
+icloudenc=$iclouddir/$vaultenc
+if [ $action = "dec" ] && [ -f $icloudenc ]
+    then
+    mv -i $icloudenc $HOME/Desktop
+fi
+
 # Check For Vault Direcory or Encrypted File On Desktop
 if [ $action = "enc" ] && [ ! -d $vaultdir ]
     then
@@ -69,11 +136,20 @@ fi
 if [ $action = 'enc' ]
     then
     tar -cf $vaultdir.tar $vaultdir && gzip $vaultdir.tar && openssl enc -base64 -e -aes-256-cbc -salt -pass pass:$hash_cfg -pbkdf2 -iter 600000 -in $vaultdir.tar.gz -out $vaultenc && rm -f $vaultdir.tar.gz
+# Move encrypted file to iCloud and vault directory to trash
+    mv -f $vaultenc $iclouddir
+    rm -rf ~/.trash/$vaultdir
+    mv -f $vaultdir ~/.trash
     echo "Vault Encrypted:" $vaultdir
+    echo "Note:  Encrypted Vault Moved To iCloud, Encrypted Directory Moved To Trash"
     echo " "
 elif [ $action = 'dec' ]
     then
     openssl enc -base64 -d -aes-256-cbc -salt -pass pass:$hash_cfg -pbkdf2 -iter 600000 -in $vaultenc -out $vaultdir.tar.gz && tar -xzf $vaultdir.tar.gz && rm -f $vaultdir.tar.gz 
+# Move encrypted file to trash
+    rm -rf ~/.trash/$vaultenc
+    mv -f $vaultenc ~/.trash
     echo "Vault Decrypted:" $vaultdir
+    echo "Note:  Encrypted Vault Moved To Trash"
     echo " "
 fi
---
+++ ShellScripts/zJsonConfig.sh
@@ -0,0 +1,25 @@
+#!/bin/zsh
+
+# THIS IS TEST CODE THAT WILL BE MERGED INTO Crypvault.sh
+
+# Search Vault/Git JSON Config. File for Filename
+str=$(cat $HOME'/Library/Mobile Documents/iCloud~is~workflow~my~workflows/Documents/Config Files/gitmgr_config.json')
+#str=$(cat zz.data)
+substr="gitmgr_name"
+#substr="vaultmgr_name"
+prefix=${str%%$substr*}
+index=${#prefix}
+nameidx=$((index + 14)) # GitMGR
+#nameidx=$((index + 16)) # VaultMGR
+
+# Create a SubString That Begins With Vault/Git Filename
+str2=$(echo $str | cut -c $nameidx-)
+
+# Strip Special Characters and Save Actual Vault/Git Filename
+filename=$(echo $str2 | cut -d '"' -f2)
+
+# Display Working Results
+clear
+echo "String:    " $str
+echo "Substring: " $str2
+echo "Filename:  " $filename
