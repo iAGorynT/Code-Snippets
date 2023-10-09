4a66bc34be4bee334900eb49b5296fac1c8aa4d3
Iam iAGorynT <108758623+iAGorynT@users.noreply.github.com>
2023-10-09 15:19:39 -0500

    Update zJQJsonConfig.sh

--- ShellScripts/zJQJsonConfig.sh
+++ ShellScripts/zJQJsonConfig.sh
@@ -2,7 +2,7 @@
 
 # Use jq Command Line JSON  Processor to Extract gitMGR Name
 file1=$HOME'/Library/Mobile Documents/iCloud~is~workflow~my~workflows/Documents/Config Files/gitmgr_config.json'
-zzz=$(jq .gitmgr_name $file1)
+zzz=$(jq -r .gitmgr_name $file1)
 
 # Display Working Results
 clear
