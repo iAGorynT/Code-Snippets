6189086e2e60a27143743504a0d426800ddadd0e
Iam iAGorynT <108758623+iAGorynT@users.noreply.github.com>
2024-03-31 19:16:36 -0500

    Create zJsonqv.sh
    Added a sample script which contains examples of using jq for viewing JSON file contents.

---
+++ ShellScripts/zTests/zJsonqv.sh
@@ -0,0 +1,24 @@
+#!/bin/zsh
+
+# JSON File Quick View Utility
+# NOTE:Pass the name of a valid JSON File when using - i.e. ./Jqv.sh json_filename.json
+
+clear
+echo "JSON File Quick View: $1..."
+echo
+
+# Pretty Print
+echo "Pretty Print..."
+jq -M '.' "$1"
+echo
+
+# Alphabetic List
+echo "Alphabetic List..."
+jq -S '.' "$1"
+echo
+
+# Reverse Alphabetic List
+echo "Reverse Alphabetic List"
+jq -S '.' "$1" | jq 'to_entries | reverse | from_entries'
+echo 
+
