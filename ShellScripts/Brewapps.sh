a7a3e0e4834eacd2c1e24df0dd8a5bfc5cd209e9
Iam iAGorynT <108758623+iAGorynT@users.noreply.github.com>
2024-05-12 11:39:00 -0500

    New Brewapps Listing
    Added new Brewapps script to list Selected Homebrew Installed Apps.

---
+++ ShellScripts/Brewapps.sh
@@ -0,0 +1,31 @@
+#!/bin/zsh
+
+# List Selected Application Descriptions
+clear
+echo "App Descriptions..."
+echo " "
+
+# Array of app names
+apps=("btop" "iperf3" "jq" "qlmarkdown" "macdown" "mosh" "speedtest" "balenaetcher" "github" "macvim")
+
+# Function to check if a line contains an app name
+contains_app() {
+    local line=$1
+    for app in "${apps[@]}"; do
+        if [[ $line == *"$app"* ]]; then
+            echo "$line"
+            break
+        fi
+    done
+}
+
+# Temporary file to store brew command results
+    temp_file=$(mktemp)
+# Run the command and store output in a temporary file
+    brew desc --eval-all $(brew list) | awk 'gsub(/^([^:]*?)\s*:\s*/,"&=")' | column -s "=" -t > $temp_file
+
+# Read file line by line and check for app names
+while IFS= read -r line; do
+    contains_app "$line"
+done < $temp_file
+
