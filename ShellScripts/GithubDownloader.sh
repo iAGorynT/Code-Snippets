296d47152d2a5ea12212ef40563278e499bd91e5
Iam iAGorynT <108758623+iAGorynT@users.noreply.github.com>
2025-02-10 11:59:16 -0600

    Update GithubDownloader.sh
    Improved usability.  Changed to accept GitHub URL and extract Owner, Repo, Branch, and Filepath.

--- ShellScripts/GithubDownloader.sh
+++ ShellScripts/GithubDownloader.sh
@@ -1,16 +1,33 @@
 #!/bin/zsh
+parse_github_url() {
+    local url="$1"
+    local github_regex='^https?://github.com/([^/]+)/([^/]+)/blob/([^/]+)/(.+)$'
+
+    if [[ "$url" =~ $github_regex ]]; then
+        printf "%s\n%s\n%s\n%s" "$match[1]" "$match[2]" "$match[3]" "$match[4]"
+    else
+        printf >&2 "Error: Invalid GitHub URL format\n"
+        printf >&2 "Usage: https://github.com/iAGorynT/Code-Snippets/blob/main/README.md\n"
+        return 1
+    fi
+}
+
 download_github_file() {
     # Early exit if curl is not installed
     command -v curl &>/dev/null || { printf >&2 "Error: curl is not installed. Please install curl to use this script.\n"; return 3; }
 
-    # Validate argument count
-    [[ "$#" -eq 4 ]] || { 
-        printf >&2 "Usage: %s owner repo branch filepath\n" "$0"
-        printf >&2 "Example: %s microsoft vscode main README.md\n" "$0"
-        return 1 
-    }
+    local github_url="$1"
+    local parsed_details
+    parsed_details=($(parse_github_url "$github_url"))
+    local ret=$?
 
-    local owner=$1 repo=$2 branch=$3 filepath=$4
+    [[ $ret -ne 0 ]] && return $ret
+
+    local owner="${parsed_details[1]}"
+    local repo="${parsed_details[2]}"
+    local branch="${parsed_details[3]}"
+    local filepath="${parsed_details[4]}"
+
     local github_raw_url="https://raw.githubusercontent.com/${owner}/${repo}/${branch}/${filepath}"
     local filename=$(basename "$filepath")
     
@@ -30,5 +47,12 @@ download_github_file() {
         return 2
     fi
 }
+
+# Main
+[[ $# -eq 0 ]] && { printf >&2 "Error: Missing GitHub URL argument\n"; exit 1; }	# Exit if no URL provided
+printf "Downloading file from GitHub...\n\n"
+
+# Usage: ./script.zsh https://github.com/owner/repo/blob/branch/filepath
 download_github_file "$@"
 exit $?
+
