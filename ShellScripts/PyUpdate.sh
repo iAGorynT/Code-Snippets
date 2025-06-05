c686e914f4929141f324c2fb8592aa8ae86cce8a
Iam iAGorynT <108758623+iAGorynT@users.noreply.github.com>
2025-06-05 11:13:44 -0500

    Update PyUpdate.sh
    Code Cleanup.

--- ShellScripts/PyUpdate.sh
+++ ShellScripts/PyUpdate.sh
@@ -75,6 +75,7 @@ else
         
         pyenv global "$latest_python" | tee -a "$LOG_FILE"
         printf "\n" | tee -a "$LOG_FILE"
+
         package_printf "Installing Python packages from requirements.txt..." | tee -a "$LOG_FILE"
         if [[ -f "$HOME/PythonCode/requirements.txt" ]]; then
             python3 -m pip install -r "$HOME/PythonCode/requirements.txt" | tee -a "$LOG_FILE" || 
@@ -83,6 +84,7 @@ else
             error_printf "Requirements file not found at $HOME/PythonCode/requirements.txt" | tee -a "$LOG_FILE"
         fi
         printf "\n" | tee -a "$LOG_FILE"
+
         success_printf "Python updated to version: $latest_python" | tee -a "$LOG_FILE"
         printf "\n"
         
